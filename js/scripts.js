c497822718ed2efc2c27c3f5debf9af9489a81bb
Yarusch <impulsimp13@gmail.com>
2023-02-24 06:37:03 +0100

    menu burger is done

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,10 @@ $(document).ready(function(){
         }
     });
 });
+
+var trigger = document.getElementById('toggle');
+var box = document.getElementById('menu');
+
+toggle.addEventListener('click', function() {
+    box.classList.toggle('active');
+});
